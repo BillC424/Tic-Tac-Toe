30aae5c0295a2ede476811899ec91e49428674f1
BillC424 <wcurran44@gmail.com>
2022-08-02 16:11:34 -0400

    Changed numbers_chosen instance variable for Player class to an empty hash with keys for each winning combination. Modified play_round method to add numbers chosen as values in the empty array values for each key the number chosen is contained in

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -4,8 +4,15 @@ class Player
     def initialize(name, player_symbol)
       @name = name
       @player_symbol = player_symbol
-      @numbers_chosen = []
-      
+      @numbers_chosen = {
+        horizontal_123: [], 
+        horizontal_456: [], 
+        horizontal_789:[],
+        vertical_147: [], 
+        vertical_258: [], 
+        vertical_369: [],
+        diagonal_159: [], 
+        diagonal_357:[]}
     end
   end
     
@@ -21,10 +28,10 @@ class Player
                          7 | 8 | 9 '.chars
     end
   end
-  
+  #Creates game board
   new_board = Board.new
   
-  ##Code for creating two players
+  #Creates two players
   puts "What is your the name of the first player?"
   p1_name = gets.chomp
   puts "Please choose X or O for your game marker"
@@ -46,31 +53,46 @@ class Player
   
   board.board_display = board.board_display.map do |number|
     if number == '1' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_123] << number
+      player.numbers_chosen[:vertical_147] << number
+      player.numbers_chosen[:diagonal_159] << number
       number = player.player_symbol
     elsif number == '2' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_123] << number
+      player.numbers_chosen[:vertical_258] << number
       number = player.player_symbol
     elsif number == '3' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_123] << number
+      player.numbers_chosen[:vertical_369] << number
+      player.numbers_chosen[:diagonal_357] << number
       number = player.player_symbol
     elsif number == '4' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_456] << number
+      player.numbers_chosen[:vertical_147] << number
       number = player.player_symbol
     elsif number == '5' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_456] << number
+      player.numbers_chosen[:vertical_258] << number
+      player.numbers_chosen[:diagonal_159] << number
+      player.numbers_chosen[:diagonal_357] << number
       number = player.player_symbol
     elsif number == '6' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_456] << number
+      player.numbers_chosen[:vertical_369] << number
       number = player.player_symbol
     elsif number == '7' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_789] << number
+      player.numbers_chosen[:vertical_147] << number
+      player.numbers_chosen[:diagonal_357] << number
       number = player.player_symbol
     elsif number == '8' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_789] << number
+      player.numbers_chosen[:vertical_258] << number
       number = player.player_symbol
     elsif number == '9' && number == player_input
-      player.numbers_chosen = player.numbers_chosen.push(number)
+      player.numbers_chosen[:horizontal_789] << number
+      player.numbers_chosen[:vertical_369] << number
+      player.numbers_chosen[:diagonal_159] << number
       number = player.player_symbol
     else  
       number = number
@@ -80,19 +102,26 @@ class Player
   puts board.board_display.join
   p player.numbers_chosen
   
-  end
+end
   
-  i = 0
+i = 0
 
-  while i < 5
+while i < 5 do
+  if i == 4 
     play_round(new_board, p1)
-    play_round(new_board, p2)
+    i +=1
+    puts "All the spots have been chosen and no one won."
+  else  
+  play_round(new_board, p1)
+  play_round(new_board, p2)
+  i += 1  
   end  
+end  
 
   
   #def game
   #each {||
-  #    { horizontal_one_thru_three: [1, 2, 3], 
+  #    { horizontal_1-3: [1, 2, 3], 
   #      horizontal_four_thru_six: [4, 5, 6], 
   #      horizontal_seven_thru_nine:[7, 8, 9],
   #      vertical_one_thru_seven: [1, 4, 7], 
